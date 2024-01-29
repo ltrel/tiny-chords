b9d2eb9b3659ac16ed2b9369b525d838e034b153
ltrel <79512656+ltrel@users.noreply.github.com>
2024-01-29 13:12:08 +1100

    Print number of bytes written

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,9 @@ int main(int argc, char * argv[])
         outName.append(".tc");
         std::ofstream outStream{outName};
         section.write(outStream);
+        std::streamoff byteCount{outStream.tellp()};
         outStream.close();
-        std::cout << fmt::format("Wrote output to: {}\n", outName);
+        std::cout << fmt::format("Wrote {} bytes to file: {}\n", byteCount, outName);
         return 0;
     }
 
@@ -52,55 +53,4 @@ int main(int argc, char * argv[])
 
     std::cout << argumentErrStr;
     return 0;
-
-    // std::cout << fmt::format("{}\n", flatten<3>({2, 3, 2}, {3, 4, 7}));
-    // std::cout << fmt::format("{}\n", collect<3>(35, {3, 4, 7}));
-
-    // std::stringstream headerStream{};
-    // SectionHeader writeHeader{138, BeatType::quarter, 4};
-    // writeHeader.write(headerStream);
-    // SectionHeader readHeader{SectionHeader::read(headerStream)};
-
-    // Chord writeChord{"F#", ChordType::maj7, 4, "Ab"};
-    // std::cout << json{writeChord} << '\n';
-
-    // std::ofstream outStream{"out.tc", std::ios::binary | std::ios::out};
-    // writeChord.write(4, outStream);
-    // outStream.close();
-
-    // std::ifstream inStream{"out.tc", std::ios::binary | std::ios::in};
-    // Chord readChord{Chord::read(4, inStream)};
-    // std::cout << readChord.print() << '\n';
-    // inStream.close();
-
-    // std::ofstream outStream{"out.tc", std::ios::binary | std::ios::out};
-    // SectionHeader header{120, BeatType::quarter, 4};
-    // Section section{header, {
-    //     {Note("C#"), ChordType::min7, header.beatCount(), Note("E")},
-    //     {Note("F#"), ChordType::dom7, header.beatCount()},
-    //     {Note("B"), ChordType::maj7, header.beatCount()},
-    // }};
-    // json j(section);
-    // std::cout << j.dump(4) << '\n';
-    // section.write(outStream);
-    // outStream.close();
-
-    // std::ifstream inStream{"out.tc", std::ios::binary | std::ios::in};
-    // Section readSection{Section::read(inStream)};
-    // for (auto &chord : readSection.chords)
-    // {
-    //     std::cout << chord.print() << '\n';
-    // }
-    // inStream.close();
-
-    // std::ifstream jsonIStream{"test.json"};
-    // json jread;
-    // jsonIStream >> jread;
-    // Section jsonReadSection{jread.template get<Section>()};
-    // for (auto &chord : jsonReadSection.chords)
-    // {
-    //     std::cout << chord.print() << '\n';
-    // }
-
-    // return 0;
 }
