d0f3a61bc9b0170f697554ffef1750bdb5b6c602
ltrel <79512656+ltrel@users.noreply.github.com>
2024-01-30 15:39:44 +1100

    Rename some variables

--- src/chord.cpp
+++ src/chord.cpp
@@ -23,24 +23,24 @@ Chord::Chord() : m_root{Note("A")}, m_chordType{ChordType::maj}, m_beats{4}, m_b
 
 Chord Chord::read(int currentDefaultDuration, std::istream &inStream)
 {
-  uint8_t current{0};
-  inStream.read(reinterpret_cast<char*>(&current), 1);
+  uint8_t first{0};
+  inStream.read(reinterpret_cast<char*>(&first), 1);
 
   std::optional<Note> bass{std::nullopt};
   // Check for slash chord prefix
-  if ((current & 1) == 0 && current > (maxShortChordIndex << 1))
+  if ((first & 1) == 0 && first > (maxShortChordIndex << 1))
   {
-    int slashChordIndex{(current >> 1) - (maxShortChordIndex)};
+    int slashChordIndex{(first >> 1) - (maxShortChordIndex)};
     std::array<int, 2> indices{collect<2>(slashChordIndex, {letterStateCount, accidentalStateCount})};
     bass = {indices[0], indices[1]};
     // Advance the input stream to the body of the chord
-    inStream.read(reinterpret_cast<char*>(&current), 1);
+    inStream.read(reinterpret_cast<char*>(&first), 1);
   }
 
   // LSb not set means short form chord
-  if ((current & 1) == 0)
+  if ((first & 1) == 0)
   {
-    int combinedIndex{current >> 1};
+    int combinedIndex{first >> 1};
     std::array<int, 3> indices{collect<3>(combinedIndex, shortChordShape)};
     Note root{indices[0], indices[1]};
     ChordType chordType{static_cast<ChordType>(indices[2])};
@@ -50,7 +50,7 @@ Chord Chord::read(int currentDefaultDuration, std::istream &inStream)
   {
     uint8_t second{0};
     inStream.read(reinterpret_cast<char*>(&second), 1);
-    int full{current + (second << 8)};
+    int full{first + (second << 8)};
     int combinedIndex{full >> 1};
     std::array<int, 4> indices{collect<4>(combinedIndex, longChordShape)};
     Note root{indices[0], indices[1]};
@@ -65,9 +65,9 @@ void Chord::write(int currentDefaultDuration, std::ostream &outStream) const
   if (m_bass.has_value())
   {
 
-    int rootIndex{m_bass->letterIndex()};
+    int letterIndex{m_bass->letterIndex()};
     int accidentalIndex{m_bass->accidentalIndex()};
-    int slashChordIndex{flatten<2>({rootIndex, accidentalIndex}, noteShape)};
+    int slashChordIndex{flatten<2>({letterIndex, accidentalIndex}, noteShape)};
     auto encoding{(maxShortChordIndex + slashChordIndex) << 1};
     outStream.write(reinterpret_cast<const char*>(&encoding), 1);
   }
