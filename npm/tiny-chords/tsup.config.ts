1795e110320089270ab39ddcd37b0d76b9c34424
ltrel <79512656+ltrel@users.noreply.github.com>
2024-02-24 14:14:59 +1100

    Optimize npm package size

--- npm/tiny-chords/tsup.config.ts
+++ npm/tiny-chords/tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/wasm.ts', 'src/nowasm.ts'],
-  splitting: false,
-  sourcemap: true,
+  sourcemap: false,
+  minify: true,
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
